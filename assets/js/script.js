2817a23fe9fa204eac71d22675f89e5b37b38c47
bazsup <supawit.ruen@mail.kmutt.ac.th>
2019-02-19 18:13:15 +0700

    :lipstick: Update styling `QA` section follow from new design on figma

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,21 +6,28 @@ function hideOtherCollapse(exceptElement) {
     let collapseItem = collapsibleList[index]
     let content = collapseItem.nextElementSibling;
     if (exceptElement !== collapsibleList[index]) {
-      collapseItem.lastElementChild.classList.remove('active')
+      collapseItem.firstElementChild.classList.remove('active')
       content.style.maxHeight = null;
+      collapseItem.style.background = null;
     }
   }
 }
 
 for (let index = 0; index < collapsibleList.length; index++) {
   collapsibleList[index].addEventListener("click", function() {
-    this.lastElementChild.classList.toggle('active')
+    this.firstElementChild.classList.toggle('active')
     let content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
+      this.style.background = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
+      this.style.background = '#E0E0E0';
       hideOtherCollapse(this)
     } 
   });
 }
+
+setTimeout(function() {
+  collapsibleList[0].click()
+}, 500)
