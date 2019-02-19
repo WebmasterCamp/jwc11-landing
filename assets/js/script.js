d43ed2df08da9ea30133e82de6e122794466e97d
bazsup <supawit.ruen@mail.kmutt.ac.th>
2019-02-19 23:45:17 +0700

    :sparkles: Add `branch` section with carousel and flipable card

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,126 @@ for (let index = 0; index < collapsibleList.length; index++) {
 setTimeout(function() {
   collapsibleList[0].click()
 }, 500)
+
+// carousel branch
+var current = 0;
+
+var positions = {
+  front: 'rotateY(0deg) translateZ(250px)',
+  right: 'rotateY(75deg) translateZ(170px)',
+  back: 'rotateY(180deg) translateZ(100px)',
+  left: 'rotateY(-75deg) translateZ(170px)'
+}
+
+function carousel(prevButton, nextButton) {
+  
+  let cards = Array.from(document.querySelectorAll('.major-card'))
+  
+  let positionList = [
+    positions.front,
+    positions.right,
+    positions.back,
+    positions.left
+  ]
+  
+  function changeStyle () {
+    cards.map((each, index) => {
+      each.style.transform = positionList[index]
+    })
+  }
+
+  function next () {
+    current = (current + 1) % 4
+    current = current === -1 ? 4 : current
+    positionList.splice(0, 0, positionList.pop())
+    changeStyle()
+  }
+
+  
+  function prev () {
+    current = (current - 1) % 4
+    current = current === -1 ? 4 : current
+    positionList.push(positionList.shift())
+    changeStyle()
+  }
+
+  this.init = function () {
+    prevElement = document.querySelector(prevButton)
+    nextElement = document.querySelector(nextButton)
+    prevElement.addEventListener('click', prev)
+    nextElement.addEventListener('click', next)
+  }
+}
+
+let branchCarousel = new carousel('#major-left', '#major-right')
+branchCarousel.init()
+
+// flip card
+
+function getViewport() {
+  var viewPortWidth
+  var viewPortHeight
+ 
+  // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+  if (typeof window.innerWidth != 'undefined') {
+    viewPortWidth = window.innerWidth,
+    viewPortHeight = window.innerHeight
+  }
+ 
+ // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+  else if (typeof document.documentElement != 'undefined'
+  && typeof document.documentElement.clientWidth !=
+  'undefined' && document.documentElement.clientWidth != 0) {
+     viewPortWidth = document.documentElement.clientWidth,
+     viewPortHeight = document.documentElement.clientHeight
+  }
+ 
+  // older versions of IE
+  else {
+    viewPortWidth = document.getElementsByTagName('body')[0].clientWidth,
+    viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
+  }
+  return [viewPortWidth, viewPortHeight];
+}
+
+
+let viewDetailButtons = Array.from(document.querySelectorAll('.side.front'))
+
+let majorCards = Array.from(document.querySelectorAll('.major-card'))
+
+
+majorCards.map(each => {
+  each.addEventListener('click', function () {
+    viewDetail(this)
+  }, false)
+})
+
+viewDetailButtons.map(eachBtn => {
+  eachBtn.addEventListener('click', function () {
+    viewDetail(this.parentElement.parentElement)
+  }, false)
+})
+
+function viewDetail(element) {
+  const branches = {
+    0: 'content',
+    1: 'design',
+    2: 'marketing',
+    3: 'programming'
+  }
+  const [widthViewport, height] = getViewport()
+  selectedBranch = element.getAttribute('data-branch')
+  if (widthViewport < 576 && branches[current] !== selectedBranch)
+    return
+
+  element.classList.add('active')
+}
+
+// detect click outside card it will hide
+document.addEventListener('click', function (event) {
+  if (!event.target.closest('.major-card')) {
+    majorCards.map(each => {
+      each.classList.remove('active')
+    })
+  }
+})
