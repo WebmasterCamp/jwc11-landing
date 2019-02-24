047aaa31d323982ef5b02e0ea10a201a67008fab
bazsup <supawit.ruen@mail.kmutt.ac.th>
2019-02-24 15:10:21 +0700

    :lipstick: Update color faq collapse

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,24 +5,29 @@ function hideOtherCollapse(exceptElement) {
   for (let index = 0; index < collapsibleList.length; index++) {
     let collapseItem = collapsibleList[index]
     let content = collapseItem.nextElementSibling;
+    let parent = collapseItem.parentElement
     if (exceptElement !== collapsibleList[index]) {
       collapseItem.firstElementChild.classList.remove('active')
       content.style.maxHeight = null;
       collapseItem.style.background = null;
+      parent.style.color = null
     }
   }
 }
-
+ 
 for (let index = 0; index < collapsibleList.length; index++) {
   collapsibleList[index].addEventListener("click", function() {
     this.firstElementChild.classList.toggle('active')
     let content = this.nextElementSibling;
+    let parent = this.parentElement
     if (content.style.maxHeight){
       content.style.maxHeight = null;
       this.style.background = null;
+      parent.style.color = null
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
       this.style.background = '#E0E0E0';
+      parent.style.color = '#0B667E'
       hideOtherCollapse(this)
     }
   });
